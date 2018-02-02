1d3b315e12c3987942da2e15520e726aae6488fb
Aslynn <RyanPhayPDX@gmail.com>
2018-02-01 18:07:43 -0800

    Here's the new project.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using QuoteMan;
 
 namespace HelloWorld
 {
@@ -11,7 +12,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello frakking world!");
+            Quotes myQuotes = new Quotes();
+            for (int i = 1; i < 25; i++)
+            {
+                Console.WriteLine(myQuotes.PrintRandomQuote());
+            }
             Thread.Sleep(5000);
         }
     }
---
+++ QuoteMan/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace QuoteMan
+{
+    public class Quotes
+    {
+        private System.String _sQuote1 = "Hello world";
+        private System.String _sQuote2 = "I am a little tea pot";
+        private System.String _sQuote3 = "Hail to the king, baby!";
+
+        public System.String PrintRandomQuote()
+        {
+            System.String Result = "SHIT";
+
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            int bob = rnd.Next(1, 4);
+
+            switch (bob)
+            {
+                case 1:
+                    Result = _sQuote1;
+                    break;
+                case 2:
+                    Result = _sQuote2;
+                    break;
+                case 3:
+                    Result = _sQuote3;
+                    break;
+            }
+
+            return Result;
+        }
+
+        public System.String ReturnQuote(int QuoteNumber)
+        {
+            System.String Result = "Fuck you!";
+
+            if (1 <= QuoteNumber && QuoteNumber <= 3)
+            {
+                switch (QuoteNumber)
+                {
+                    case 1:
+                        Result = _sQuote1;
+                        break;
+                    case 2:
+                        Result = _sQuote2;
+                        break;
+                    case 3:
+                        Result = _sQuote3;
+                        break;
+                }
+            }
+
+            return Result;
+        }
+
+        public System.Int16 QuoteCount
+        {
+            get { return 3; }
+        }
+    }
+}
